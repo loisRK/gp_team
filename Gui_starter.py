abb5b5e9360c2c46642dc8a4920b9dd24eb546e8
Giri_s <riccess1245@gmail.com>
2022-09-20 12:10:16 +0900

    제 개인 컴퓨터에 맞춰 설정되어 있던 것을 다른 팀원분들도 사용할 수 있도록 설정을 변경하였습니다.

--- Gui_starter.py
+++ Gui_starter.py
@@ -5,7 +5,7 @@ import sys
 from PyQt5 import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from After_Page_Gui_Test import Ui_MainWindow #앞의 파일명 동일 kinwriter_python 만 변경
+from Page_Gui_Test import Ui_MainWindow #앞의 파일명 동일 kinwriter_python 만 변경
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium import webdriver
@@ -13,7 +13,7 @@ import time
 import pyperclip
 from bs4 import BeautifulSoup
 import pyautogui
-from Crawling_NaverMap import Find_Store
+from Crawling_Store import Find_Store
 
 class auto_w(QMainWindow,Ui_MainWindow): #class name 변경
     def __init__(self):
