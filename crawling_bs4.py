8edee82897c0c0f0b5bc71b810e57df674fca90c
Giri_s <riccess1245@gmail.com>
2022-09-23 16:54:03 +0900

    if 문제 해결 [문제 원인] 1. 더보기란을 누르는 if문이 하드코딩으로 작성하였기 때문에 특정 상황에서 오류가 발생함 2. 검색하는 가게가 존재하지 않을 경우의 실행문이 없어서 오류가 발생함 [해결] 1. 더보기란의 if문을 for문을 활용하여 모든 상황에 대응할 수 있도록 수정 2. 존재하지 않은 가게를 입력할 경우 발생한 문제를 if문으로 해결할 수 있도록 수정

--- crawling_bs4.py
+++ crawling_bs4.py
@@ -6,21 +6,22 @@ from bs4 import BeautifulSoup
 
 
 class Find_Store2():
+    global Star_Total
+    global Comment_Total
 
     def __init__(self):
         super().__init__()
-        self.Comment_Total = None
-        self.Store_Name = None
-        self.Star_Total = None
-        self.Positive_Review = None     # 모델학습결과 넣을 변수
-        self.Negative_Review = None     # 모델학습결과 넣을 변수
+        self.Store_Name = ""
+        self.Store_Address = ""
+        self.Star_Total = ""
+        self.Review_Count = ""
+        self.Positive_Review = ""
 
     def play(self, sname):
 
         global review_list
         global star_list
         global menu_list
-        global star_total_list
 
         # 크롤링 작업
         Store_link = "https://www.yogiyo.co.kr/mobile/#/"
@@ -43,96 +44,114 @@ class Find_Store2():
         time.sleep(3)
         print('test')
 
-        # 첫번째 가게 클릭
-        driver.find_element_by_xpath('//*[@id="content"]/div/div[5]/div/div/div[1]/div/table/tbody/tr/td[2]/div/div[1]').click()
-        time.sleep(3)
 
-        # 클린댓글 클릭
-        driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/ul/li[2]/a').click()
-        count = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/ul/li[2]/a/span').text
-        time.sleep(3)
+        # 가게 여부를 확인해주는 변수 check_store
+        check_store = driver.find_element_by_xpath('//*[@id="content"]/div/div[7]').text == ''
 
-        # 더보기 클릭하여 댓글 30개 펼치기
-        if driver.find_element_by_xpath('//*[@id="review"]/li[12]/a').is_enabled():
-            driver.find_element_by_xpath('//*[@id="review"]/li[12]/a').click()
+        #test-----------------------------------------------------------------------------------------
+        if check_store == False:
+            print('가게가 없을 경우')
+            driver.quit()
+        elif check_store == True:
+            print("가게가 있을 경우")
+            # --------------------------------------------------------------------------------------------
+
+            # 첫번째 가게 클릭
+            driver.find_element_by_xpath('//*[@id="content"]/div/div[5]/div/div/div[1]/div/table/tbody/tr/td[2]/div/div[1]').click()
             time.sleep(3)
-        else:       # 댓글이 10개 미만인 경우 에러가 발생하지 않도록 pass
-            pass
 
-        if driver.find_element_by_xpath('//*[@id="review"]/li[22]/a').is_enabled():
-            driver.find_element_by_xpath('//*[@id="review"]/li[22]/a').click()
+            # 클린댓글 클릭
+            driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/ul/li[2]/a').click()
+            count = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/ul/li[2]/a/span').text
             time.sleep(3)
-        else:
-            pass
 
-        if driver.find_element_by_xpath('//*[@id="review"]/li[32]/a').is_enabled():
-            driver.find_element_by_xpath('//*[@id="review"]/li[32]/a').click()
-            time.sleep(3)
-        else:
-            pass
+            # 가게 정보 긁어오기
+            self.Star_Total = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div[5]/div[1]/div/div/strong').text
+            self.Comment_Total = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/ul/li[2]/a/span').text
 
-        print('test2')
+            # 더보기 클릭하여 댓글 30개 펼치기----------------------------------------------------------------------------------
+            # if driver.find_element_by_xpath('//*[@id="review"]/li[12]/a').is_enabled():
+            #     driver.find_element_by_xpath('//*[@id="review"]/li[12]/a').click()
+            #     time.sleep(3)
+            # else:       # 댓글이 10개 미만인 경우 에러가 발생하지 않도록 pass
+            #     pass
+            #
+            # if driver.find_element_by_xpath('//*[@id="review"]/li[22]/a').is_enabled():
+            #     driver.find_element_by_xpath('//*[@id="review"]/li[22]/a').click()
+            #     time.sleep(3)
+            # else:
+            #     pass
+            #
+            # if driver.find_element_by_xpath('//*[@id="review"]/li[32]/a').is_enabled():
+            #     driver.find_element_by_xpath('//*[@id="review"]/li[32]/a').click()
+            #     time.sleep(3)
+            # else:
+            #     pass
+            #--------------------------------------------------------------------------------------------------------------
 
-        # 가게 정보 긁어오기
-        self.Star_Total = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div[5]/div[1]/div/div/strong').text
-        self.Comment_Total = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/ul/li[2]/a/span').text
-        self.Store_Name = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div[1]/div[1]/span').text
+            print("더보기란 클릭 시작")
+            # 여기서 수정한 if문 넣기------------------------------------------------------------------------------------------
+            # 댓글의 인덱싱은 xpath의 경로 중 li[]에서 [] 안에 있는 숫자로 결정됨
+            # 시작 인덱싱은 2부터 시작
+            # 한번에 보여줄 수 있는 댓글의 갯수는 10개이며 넘어갈 경우 더보기란 생성
+            # 더보기를 누를 때마다 보여지는 댓글이 10개씩 추가됨
+            # 댓글의 갯수가 x라는 변수로 지정하였을 때 더보기 버튼의 총 갯수는 x/10
 
-        # selenium 작업으로 더보기 펼친 후 html 긁어오기
-        html = driver.page_source
-        print('html')
+            reviewtotal = int(self.Comment_Total)
+            origin_more_click_count = reviewtotal / 10 - 1
+            # 원하는 댓글의 수를 입력하면  그 갯수에 맞춰 더보기를 클릭할 수 있도록 설정하기(지금은 임의로 30개를 긁어올 수 있도록 2으로 설정)
+            # 2 대신 넣어야 하는 것(입력값의 변수 = x)  =>  x/10 - 1
+            more_click_count = 2
 
-        soup = BeautifulSoup(html, 'html.parser')
-        # id=review 인 ul 태그 가져오기
-        review_tag = soup.select_one("ul.list-group.review-list")
-        # print(review_tag)
+            if reviewtotal < 11:
+                driver.quit()
+                print("댓글이 없거나 너무 적습니다.")
+            elif reviewtotal > 10:  # 이 때부터 더보기란 생성
+                for i in range(1, more_click_count + 1):
+                    driver.find_element_by_xpath(f'//*[@id="review"]/li[{i * 10 + 2}]/a').click()
+                    time.sleep(3)
+                    # return print("Success")   # for문에서 나올 때 오류가 발생하며 비정상종료됐지만 return 값이 생기니깐 비정상 종료는 안됨
+                                                # return을 사용하면 종료가 되는 것이기 때문에 결국 원점
+                    if i == more_click_count:
+                        break                   # break로 해결됨
+                # print('TEEEEEST')             # 하지만 이후 실행되야할 명령어들이 실행되지 않고 끝이 남
+                    break
+            print('더보기란 클릭 완료')
+            #--------------------------------------------------------------------------------------------------------------
 
-        review_list = []
-        star_list = []
-        menu_list = []
+            # selenium 작업으로 더보기 펼친 후 html 긁어오기
+            html = driver.page_source
+            print('html 수집')
 
-        # # ul tag 중 원하는 tag 가져오기
-        # for p in review_tag.select('p.ng-binding'):
-        #     review_list.append(p.get_text())
-        # print(review_list)
+            soup = BeautifulSoup(html, 'html.parser')
+            # id=review 인 ul 태그 가져오기
+            review_tag = soup.select_one("ul.list-group.review-list")
+            # print(review_tag)
 
-        for p in review_tag.select('p.ng-binding')[0]:
-            rt_ = p.find('p')
-            review_list.append(rt_['ng-bind-html=review.comment|strip_html'])
-        print(review_list)
-
-        for div in review_tag.select('div.order-items.default.ng-binding'):
-            menu_list.append(div.get_text())
-        print(menu_list)
-
-        for s in review_tag.select('div.star-point > span.total'):
-            star = ""
-            for st in s.select('span.full.ng-scope'):
-                star += st.get_text()
-            star_list.append(star)
-        print(star_list)
-
-        # # total star
-        # for ss in review_tag.select('span'):
-        #     star_total_list.append(ss.select_one('.full.ng-scope'))
-        # print(star_total_list)
-
-
-        # all_review_comment = review_tag.select('#review > li:nth-child(2) > p')
-        # print(type(all_review_comment))
-        # all_star_comment = review_tag.select('#review > li:nth-child(2) > div:nth-child(2)')
-        # print(type(all_star_comment))
-        # all_menu_comment = review_tag.select('#review > li:nth-child(2) > div.order-items.default.ng-binding')
-        # print(all_menu_comment)
+            review_list = []
+            star_list = []
+            menu_list = []
+            # ul tag 중 원하는 tag 가져오기
+            for p in review_tag.select('p.review.comment'):
+                review_list.append(p)
+                print(review_list)
+            # all_review_comment = review_tag.select('#review > li:nth-child(2) > p')
+            # print(type(all_review_comment))
+            # all_star_comment = review_tag.select('#review > li:nth-child(2) > div:nth-child(2)')
+            # print(type(all_star_comment))
+            # all_menu_comment = review_tag.select('#review > li:nth-child(2) > div.order-items.default.ng-binding')
+            # print(all_menu_comment)
 
 
-        # beautifulsoup_html 정보 분석
-        # beautifulsoup_select_one() 함수
-        # review_list = soup.findAll('span', class_='full ng-scope')
+            # beautifulsoup_html 정보 분석
+            # beautifulsoup_select_one() 함수
+            # review_list = soup.findAll('span', class_='full ng-scope')
 
-        # print(review_list)
-        # print(star_list)
-        # print(menu_list)
+            # print(review_list)
+            # print(star_list)
+            # print(menu_list)
+
+            driver.quit()
 
     def print_review(self):
         return review_list, star_list, menu_list
