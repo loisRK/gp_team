95ecb60dd954b67555ab548bf1210bab94e7dc7a
RK <loiskim150@gmail.com>
2022-09-23 20:37:09 +0900

    [GUI] GUI 수정 - 화면 분리

--- Gui_starter2.py
+++ Gui_starter2.py
@@ -8,28 +8,54 @@ import os
 import sys
 from data_preprocessing import data_frame
 
+
 # 파일 불러오는 함수 생성
 def resource_path(relative_path):
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
+#################main.ui 가져오기#######################
+form = resource_path('main.ui')
+form_class1 = uic.loadUiType(form)[0]
 #################Page_Gui_Test.ui 가져오기#######################
-form = resource_path('Page_Gui_Test.ui')
-form_class = uic.loadUiType(form)[0]
+form2 = resource_path('content.ui')
+form_class2 = uic.loadUiType(form2)[0]
 
-
-class auto_w(QMainWindow,form_class): #class name 변경
+class auto_main(QMainWindow, form_class1):
+    # global store_name
     def __init__(self):
         super().__init__()
-
         self.setupUi(self)
+        # self.show()
+
+        self.Input_Store.setText('음식점 이름을 입력하세요.')
+        self.Find_Store_Btn.clicked.connect(self.get_store)
+        self.Find_Store_Btn.clicked.connect(self.initUi)
+
+    def get_store(self):
+        global store_name
+        store_name = self.Input_Store.text()
+
+    def initUi(self):
+        # 크롤링 코드에 입력 값 넣어줌
+        FS = Find_Store2()
+        FS.play(store_name)
+        self.hide() # 메인 윈도우 숨김
+        self.second = auto_w()
+        self.second.exec()      # 두 번째 창 닫을 때 까지 기다림
         self.show()
 
-    # 실행에 필요한 모든 코드들은 전부 start() 함수에 모두 삽입
-    def start(self):
 
+class auto_w(QWidget,form_class2):      # class name 변경
+    def __init__(self):
+        super(auto_w, self).__init__()
+        self.setupUi(self)
+        self.show()
+        self.start_GP()
+
+    # 실행에 필요한 모든 코드들은 전부 start() 함수에 모두 삽입
+    def start_GP(self):
         FS = Find_Store2()
-        FS.play(self.Input_Store.text())
 
         # GUI : 빈칸 채우기 반환값
         self.Store_Name.setText(FS.Store_Name)    # GUI: 가게이름
@@ -37,17 +63,17 @@ class auto_w(QMainWindow,form_class): #class name 변경
         self.Star_Total.setText(FS.Star_Total)  # GUI: 총 평점
 
         review, menu, star_t, star_opt = FS.print_review()
+        # review = data_frame()
+        # review.review_pre(review)
+        # review.menu_pre(menu)
+        # review.star_pre(star_t)
+        #
+        # review.make_csv(review, menu, star_t, star_opt)
 
 
-        review = data_frame()
-        review.review_pre(review)
-        review.menu_pre(menu)
-        review.star_pre(star_t)
-
-        review.make_csv(review, menu, star_t, star_opt)
-
-
-app = QApplication([])
-main_dialog = auto_w() #해당부분 위 class name과 동일하게 작성
-QApplication.processEvents()
-app.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_dialog = auto_main() #해당부분 위 class name과 동일하게 작성
+    main_dialog.show()
+    QApplication.processEvents()
+    app.exit(app.exec_())
--- crawling_bs4.py
+++ crawling_bs4.py
@@ -12,14 +12,16 @@ class Find_Store2():
         self.Comment_Total = None
         self.Store_Name = None
         self.Star_Total = None
-        self.review_list = None     # 데이터 전처리용 변수
-        self.star_list = None       # 데이터 전처리용 변수
-        self.menu_list = None       # 데이터 전처리용 변수
-        self.star_opt = None        # 데이터 전처리용 변수
+
         self.Positive_Review = None     # 모델학습결과 넣을 변수
         self.Negative_Review = None     # 모델학습결과 넣을 변수
 
     def play(self, sname):
+
+        global review_list
+        global star_list
+        global menu_list
+        global star_opt
 
         # 크롤링 작업
         Store_link = "https://www.yogiyo.co.kr/mobile/#/"
@@ -79,7 +81,6 @@ class Find_Store2():
         # selenium 작업으로 더보기 펼친 후 html 긁어오기
         html = driver.page_source
         print('html')
-        print(html)
 
         soup = BeautifulSoup(html, 'html.parser')
         # id=review 인 ul 태그 가져오기
@@ -115,4 +116,4 @@ class Find_Store2():
         print(star_opt)
 
     def print_review(self):
-        return self.review_list, self.menu_list, self.star_list, self.star_opt
+        return review_list, menu_list, star_list, star_opt
