2befe0eccbc5f3b470964b1cf8337377dad78365
hwch0 <hyewon0710@gmail.com>
2022-09-26 19:14:41 +0900

    - 예측 평점과 실제 평점 일치율 파이차트 추가

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,13 +1,20 @@
 import pandas as pd
+import numpy as np
+from konlpy.tag import Okt
+from collections import Counter
+from wordcloud import WordCloud
+import matplotlib
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 from matplotlib_font import font_setting
 import re
 import matplotlib.pyplot as plt
 from konlpy.tag import Okt
 from collections import Counter
 from wordcloud import WordCloud
+import math
 
 font_setting()
-
 
 class data_frame:
     def __init__(self):
@@ -17,7 +24,7 @@ class data_frame:
 
     def make_csv(self, review, menu, star_t, star_opt):
         global store_df
-        data = {'review': review, 'menu': menu, 'star_total': star_t, 'star_option': star_opt}
+        data = {'review' : review, 'menu' : menu, 'star_total' : star_t, 'star_option' : star_opt}
         store_df = pd.DataFrame(data)
         store_df.to_csv('C:/Users/Playdata/project/data.csv')
         return store_df
@@ -62,8 +69,7 @@ class data_frame:
                     count_list.append(int(num[0]))
 
         menu_df = pd.DataFrame({'menu': menu_list, 'count': count_list}, index=None)
-        menu_sorted = pd.DataFrame(data=menu_df.groupby('menu').sum().sort_values(by='count', ascending=False),
-                                   index=None)
+        menu_sorted = pd.DataFrame(data=menu_df.groupby('menu').sum().sort_values(by='count', ascending=False),index=None)
         menu_sorted['rank'] = menu_sorted['count'].rank(method='min', ascending=False)
         menu_sorted.reset_index(inplace=True)
 
@@ -77,5 +83,19 @@ class data_frame:
 
         colors = ['#f7ecb0', '#ffb3e6', '#99ff99', '#66b3ff', '#c7b3fb', '#ff6666', '#f9c3b7']
         plt.pie(ratio, labels=labels, autopct='%.1f%%', colors=colors, explode=explode, shadow=True)
-        print(type(plt.show()))
+        return plt.show()
+
+    def math_pie(self, star_t, pred):
+        star = [len(s) for s in star_t]  # 별 개수 세서 리스트로 저장
+        pred_trunc = list(map(math.trunc, pred))  # 예측 결과 소수점 이하 버림
+
+        result = [s == p for s, p in zip(star, pred_trunc)]  # 일치 여부 비교
+        ratio = [result.count(True), result.count(False)]  # 일치율, 불일치율 저장
+
+        labels = ['일치율', '불일치율']
+        ratio = ratio
+
+        explode = [0.05 for i in range(len(labels))]
+        colors = ['#f7ecb0', '#ffb3e6', '#99ff99', '#66b3ff', '#c7b3fb', '#ff6666', '#f9c3b7']
+        plt.pie(ratio, labels=labels, autopct='%.1f%%', colors=colors, explode=explode, shadow=True)
         return plt.show()
